fe1e80a843e2f82206d95719b3b7d296a50069f0
Felipe9814 <fehh.rr@gmail.com>
2025-09-27 18:19:43 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,38 +7,35 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 import java.util.Scanner;
-public class Main
-{
-	public static void main(String[] args) {
-		Scanner leia = new Scanner (System.in);
-	
-		System.out.println("opção 1_calcular o perímetro do círculo.\n ");
-		System.out.println("opção 2_calcular a área do círculo. \n");
-		System.out.println("opção 3_calcular o volume da esfera. \n");
-		System.out.println("Digite uma opção de calculo: 1 , 2 , 3 :");
-		int opcao = leia.nextInt();
-	    System.out.println("Digite o valor do raio :");
-	    double raio = leia.nextDouble();
-	    
-	    switch (opcao) { 
-	        case 1: 
-	        double peri = 2* Math.PI*raio;
-	        System.out.printf("perimetro = %.2f \n" , peri);
-	        break;
-	        case 2: 
-	        double area = Math.PI * Math.pow(raio,2);
-	        System.out.printf("Área = %.2f \n" , area);
-	        break;
-	        case 3: 
-	        double volume = 4.0/3.0 * Math.PI * Math.pow(raio,3);
-	        System.out.printf("volume = %.2f \n", volume);
-	        break;
-	    default:
-	        System.out.println("Errou!!!");
-	    }
-	leia.close();    
-	    
-	    
-	    
-	}
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        Random sorteio = new Random();
+
+        
+        System.out.print("Digite o primeiro número inteiro: ");
+        int n1 = ler.nextInt();
+
+        System.out.print("Digite o segundo número inteiro: ");
+        int n2 = ler.nextInt();
+
+        
+        int menor = Math.min(n1, n2);
+        int maior = Math.max(n1, n2);
+        
+               int numeroSorteado = sorteio.nextInt(maior - menor + 1) + menor;
+
+        System.out.println("Número sorteado: " + numeroSorteado);
+
+      
+        if (numeroSorteado % 2 == 0) {
+            System.out.println("O número é PAR.");
+        } else {
+            System.out.println("O número é ÍMPAR.");
+        }
+
+        ler.close();
+    }
 }
